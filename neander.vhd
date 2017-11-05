168623a4d626fcdb08d011ac3a4f34f054f3d7d4
tbvinicius <32527904+tbvinicius@users.noreply.github.com>
2017-11-04 22:57:49 -0200

    Add files via upload

--- neander.vhd
+++ neander.vhd
@@ -34,9 +34,32 @@ end neander;
 
 architecture Behavioral of neander is
 
+signal clkMain : in std_logic;
+signal ResetMain : in std_logic;
+signal CargaAC ; : in std_logic;
+
+signal Sai_Ula_EntraAC             : in std_logic_vector(7 downto 0);
+signal Sai_AC_entra_X_e_Mux2       : in std_logic_vector(7 downto 0);
+signal Sai_RDM_Entra_Y_PC_Mux1_RI  : in std_logic_vector(7 downto 0);
+signal Sai_PC_Entra_Mux1  : in std_logic_vector(7 downto 0);
+
+
+COMPONENT reg8
+   PORT(
+		carga : in std_logic_vector (1 downto 0);
+		clk, rst, cargaReg : in std_logic;
+		output : out std_logic_vector (1 downto 0)
+		);
+ END COMPONENT;
 
 begin
-
+	Acumulador: reg8 work.cpu
+		PORT MAP( 
+			carga => Sai_Ula_EntraAC,
+			clk => clkMain,
+			rst => ResetMain,
+			cargaReg => CargaAC,
+			output => Sai_AC_entra_X_e_Mux2
+			);
 
 end Behavioral;
-
